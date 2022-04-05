6b0c994de9470eb175e408939e05ec0b90312eb0
Kevin <joankmr07@gmail.com>
2022-04-05 18:50:57 -0500

    proyecto de aula sistema de menu para restaurantes  parte1

---
+++ SistemadeMenus/SistemadeMenus/Menu.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemadeMenus
+{
+    internal class Menu
+    {
+        #region atributos
+        private string _nombrePlato;
+        private string _descripcion;
+        private float _precio;
+        List<Menu> _menus = new List<Menu>();
+
+        #endregion
+
+
+        #region constructor
+        public string NombrePlato { get => _nombrePlato; set => _nombrePlato = value; }
+        public string Descripcion { get => _descripcion; set => _descripcion = value; }
+        public float Precio { get => _precio; set => _precio = value; }
+        #endregion
+
+
+        #region metodos
+        public void agregarPlato(string nombre,string descripcion, float precio)
+        {
+            _menus.Add(new Menu() { NombrePlato = nombre, Descripcion = descripcion,Precio= precio});
+        }
+
+
+        public void removerPlato(string nombre)
+        {
+            for(var i = 0; i < _menus.Count; i++)
+            {
+                if(_menus[i].NombrePlato == nombre)
+                {
+                    _menus.Remove(_menus[i]);
+                }
+            }
+        }
+
+        public void actualizarPLato(string nombre, float precio)
+        {
+            for (var i = 0; i < _menus.Count; i++)
+            {
+                if (_menus[i].NombrePlato == nombre)
+                {
+                    _menus[i].Precio = precio;
+                }
+            }
+        }
+
+        public void mostrarMenu()
+        {
+            foreach(var menu in _menus)
+            {
+                Console.WriteLine(menu);
+            }
+        }
+
+        #endregion
+
+
+
+    }
+}
---
+++ SistemadeMenus/SistemadeMenus/Restaurante.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemadeMenus
+{
+    internal class Restaurante
+    {
+        static void Main(String[] arg)
+        {
+            #region atributos
+            int _numeroMesa;
+            float _cuenta;
+            float _totalPagar;
+            string nombre;
+            string descripcion;
+            float precio;
+            int opcion;
+            Menu _plato = new Menu();
+
+
+            Console.WriteLine("Que desea hacer?");
+            Console.WriteLine("opcion 1: Desea ver el menu?");
+            Console.WriteLine("opcion 2: Desea agregar un  nuevo plato al menu?");
+            Console.WriteLine("opcion 3: Desea eliminar un plato del menu?");
+            Console.WriteLine("opcion 4: Desea actualizar el precio de algun plato en el menu?");
+            opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    _plato.mostrarMenu();
+                    break;
+
+                case 2:
+                    Console.WriteLine("opcion 2: agregar un  nuevo plato al menu?");
+                    Console.WriteLine("porfavor indique nombre, descripcion y precio del nuevo plato porfavor");
+                    nombre = Console.ReadLine();
+                    descripcion = Console.ReadLine();
+                    precio = Convert.ToInt32(Console.ReadLine());
+                    _plato.agregarPlato(nombre,descripcion,precio);
+                    break;
+
+                case 3:
+                    Console.WriteLine("opcion 3: eliminar un plato del menu?");
+                    Console.WriteLine("Indique acontinuacion el plato que desea retirar del menu actual");
+                    nombre = Console.ReadLine();
+                    _plato.removerPlato(nombre);
+                    break;
+
+                case 4:
+                    Console.WriteLine("opcion 4: actualizar el precio de algun plato en el menu?");
+                    Console.WriteLine("por favor ingrese el nombre del plato");
+                    nombre = Console.ReadLine();
+                    Console.WriteLine("Acontinuacion ingrese el nuevo valor del plato");
+                    precio= Convert.ToInt32(Console.ReadLine());
+                    _plato.actualizarPLato(nombre,precio);
+                    break;
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+#endregion
